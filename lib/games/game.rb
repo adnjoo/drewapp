a0cb412f0f0a3f7eb8effc6cd59d823fa2df888c
Andrew Njoo <adnjoo@gmail.com>
2024-09-17 12:33:09 -0500

    fix

--- lib/games/game.rb
+++ lib/games/game.rb
@@ -1,28 +1,24 @@
-# Simple number guessing game
+class NumberGuessingGame
+  attr_reader :target_number, :tries, :max_tries
 
-# Set the target number to guess
-target_number = rand(1..100)
-tries = 0
-max_tries = 5
-
-puts "Welcome to the number guessing game!"
-puts "You have #{max_tries} tries to guess the correct number between 1 and 100."
-
-while tries < max_tries
-  print "Enter your guess: "
-  guess = gets.chomp.to_i
-  tries += 1
-
-  if guess == target_number
-    puts "Congratulations! You guessed the number in #{tries} tries."
-    break
-  elsif guess < target_number
-    puts "Too low!"
-  else
-    puts "Too high!"
+  def initialize
+    @target_number = rand(1..100)
+    @tries = 0
+    @max_tries = 5
   end
 
-  if tries == max_tries
-    puts "Sorry, you've used all your tries. The correct number was #{target_number}."
+  def guess(guess)
+    @tries += 1
+    return :correct if guess == target_number
+    return :too_low if guess < target_number
+    :too_high
+  end
+
+  def game_over?
+    tries >= max_tries
+  end
+
+  def correct_answer
+    target_number
   end
 end
