1b71993ead2eb3052671cc9d14bd18d5eaf0df66
Andrew Njoo <adnjoo@gmail.com>
2024-09-17 13:19:15 -0500

    wip

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,26 @@
 class GamesController < ApplicationController
   def index
+    @target_number = rand(1..100)
+    @tries = 0
+    @max_tries = 5
+  end
+
+  def guess
+    @target_number = params[:target_number].to_i
+    @guess = params[:guess].to_i
+    @tries = params[:tries].to_i + 1
+    @max_tries = 5
+
+    if @guess == @target_number
+      @message = "Congratulations! You guessed the number in #{@tries} tries."
+    elsif @tries == @max_tries
+      @message = "Sorry, you've used all your tries. The correct number was #{@target_number}."
+    elsif @guess < @target_number
+      @message = "Too low!"
+    else
+      @message = "Too high!"
+    end
+
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # game logic
   get "games/index"
+  post "games/guess", to: "games#guess"
+
+  # articles
   root "articles#index"
 
   resources :articles
