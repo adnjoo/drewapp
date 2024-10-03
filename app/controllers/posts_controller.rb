503d54e75caa3326276af5f1f1f08efc182b1080
Andrew Njoo <adnjoo@gmail.com>
2024-10-03 06:39:35 -0500

    controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+  WORDPRESS_SITE_URL = "https://andrewnjoo.com"
+
   def show
+    post_id = params[:id]
+    response = HTTP.get("#{WORDPRESS_SITE_URL}/wp-json/wp/v2/posts/#{post_id}")
+    @post = JSON.parse(response.body)
   end
 end
