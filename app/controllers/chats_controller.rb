c7677b5b3a9bdcc760be2dee0fd1bc3270ebac94
Andrew Njoo <adnjoo@gmail.com>
2024-10-04 21:36:32 -0500

    🧼

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,10 +6,7 @@ class ChatsController < ApplicationController
   def create
     service = OpenaiService.new
     @response = service.chat(params[:prompt])
-    # Log response to verify
-    Rails.logger.info("OpenAI Response: #{@response}")
 
-    # Re-render the form with @response
     render :new
   end
 end
--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -6,13 +6,4 @@ class ChatsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "form"
   end
-
-  test "should create chat and get response" do
-    skip "Skipping this test for now"
-    post chats_create_url, params: { prompt: "test prompt" }
-
-    assert_response :success
-    assert_select "h3", "Response:"
-    assert_select "p", /.*./
-  end
 end
