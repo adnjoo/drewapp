51f2437c510e27fb44c18106cd3ce4fe55808193
Andrew Njoo <adnjoo@gmail.com>
2024-10-07 21:56:24 -0500

    👋

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,11 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "name" ]
+  static targets = [ "name", "element" ]
 
   greet() {
     const element = this.nameTarget
     const name = element.value
     console.log(`Hello, ${name}!`)
+  }
+
+  toggle() {
+    this.elementTargets.forEach(target => {
+      target.classList.toggle("hidden");
+    });
   }
 }
