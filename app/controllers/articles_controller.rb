baf52aeb49b4bb8cc89d4057a081c5dd9f56176a
Andrew Njoo <adnjoo@gmail.com>
2024-10-07 22:37:23 -0500

    migration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
+  before_action :authenticate_user!
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+  before_action :authorize_user!, only: [ :edit, :update, :destroy ]
 
   def index
     @articles = Article.all
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
 
     if @article.save
       redirect_to @article
@@ -24,12 +26,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to @article
     else
@@ -38,9 +37,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-
     redirect_to root_path, status: :see_other
   end
 
@@ -48,4 +45,14 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :body)
     end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def authorize_user!
+    if @article.user != current_user
+      redirect_to articles_path, alert: "You are not authorized to perform this action."
+    end
+  end
 end
