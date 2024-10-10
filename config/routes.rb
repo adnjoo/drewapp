49879186e34607a6190203f5373b37fa6af0a9c2
Andrew Njoo <adnjoo@gmail.com>
2024-10-10 07:32:18 -0500

    rails g controller LandingPage index

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get "games/index"
-  post "games/guess", to: "games#guess"
-
-  root "articles#index"
+  # Marketing
+  root "landing_page/index"
+  # Blog
   resources :articles
 
-  resources :posts, only: [ :index, :show ]
-
+  # AI
   post "chats/create", to: "chats#create"
   get "chats/home", to: "chats#new", as: :chats_home
 
+  # API
   namespace :api do
     get "hello", to: "api#hello"
     resources :articles, only: [ :index, :show ]
